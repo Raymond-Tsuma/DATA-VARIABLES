54323da38eab10fb571c8fd32af3f02d40a222cb
Raymond-Tsuma <raymondndoro920@gmail.com>
2025-02-21 06:49:28 -0600

    Update raymond.c.c

--- raymond.c.c
+++ raymond.c.c
@@ -5,8 +5,11 @@ ADM NO : D33-2402-2023
 #include<stdio.h>
 
 int main(){
+// use of float if decimal values are used i.e %f
+// use of int for whole numbers i.e %d
     int length, width,area;
     printf("Program to find the area of rectangle: \n"); 
+
     printf("Enter the length: \n");
     scanf("%d",&length);
 
