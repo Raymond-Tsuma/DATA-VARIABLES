add34575cbb6994dee79b14aa10863c96fde71a9
Raymond-Tsuma <raymondndoro920@gmail.com>
2025-02-21 13:01:31 +0300

    Update raymond.c

--- raymond.c.c
+++ raymond.c.c
@@ -9,9 +9,12 @@ int main(){
     printf("Program to find the area of rectangle: \n"); 
     printf("Enter the length: \n");
     scanf("%d",&length);
+
     printf("Enter the width:\n");
     scanf("%d",&width);
+
     area=length*width;
+
     printf("The area is %d", area);
     return 0;
     
